0a633da0acd4dfba7e736c2472a91bb99c50b900
yixiu <mail@lvyabin.com>
2024-07-14 10:25:16 +0800

    修改uuid为空的bug

--- vless.sh
+++ vless.sh
@@ -9,6 +9,7 @@ check_pm2_installed() {
     fi
 }
 
+
 # Function to install pm2
 install_pm2() {
     if ! check_pm2_installed; then
@@ -18,12 +19,37 @@ install_pm2() {
         echo "pm2已经安装。"
     fi
 }
+# Function to generate a UUID
+generate_uuid() {
+    for i in {1..3}; do
+        uuid=$(uuidgen)
+        if [[ -n "$uuid" ]]; then
+            echo "$uuid"
+            return
+        fi
+    done
 
+    # 预定义的UUID列表
+    predefined_uuids=(
+        "fb210b24-46dd-4b4c-92ce-097385945dad"
+        "53cfcb07-8c25-4c25-baaa-95b4b50871a2"
+        "445ae56f-727d-495e-9c88-cbe942d144a6"
+        "078eb39d-2094-4272-b221-782ba0520dd6"
+        "5826e9cc-c5b7-49ca-8b37-a0ea68f382cc"
+        "e79fda4a-9519-4ef3-8973-130801b3d0ae"
+        "c0422b3b-00aa-4dbe-8573-6fb15d49e557"
+        "907e3ac9-02de-47fe-b40c-c2bd912c3adf"
+        "c53ca34c-8d9c-4a7e-8b44-5da52e4b5eaa"
+        "73fc0a2d-2458-435b-92aa-b4e8e3e40944"
+    )
+    uuid=${predefined_uuids[$RANDOM % ${#predefined_uuids[@]}]}
+    echo "$uuid"
+}
 # Function to save config.json
 save_config() {
     local port=$1
     if [[ ! -f ./vless/config.json ]]; then
-        uuid=$(uuidgen)
+        uuid=$(generate_uuid)
         cat <<EOL > ./vless/config.json
 {
     "uuid": "$uuid",
