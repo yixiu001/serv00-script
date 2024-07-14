7752d49e4fbeb38c62e855ce58becee88bc609a2
yixiu <mail@lvyabin.com>
2024-07-14 20:39:21 +0800

    修改脚本内容

--- vless.sh
+++ vless.sh
@@ -66,19 +66,12 @@ EOL
 
 # Function to deploy vless
 deploy_vless() {
-    local port=${1:-3000}  # Default port is 3000 if not provided
-    # 修改端口号
-    save_config "$port"
     # 赋予vless维护脚本权限
     chmod +x ./vless/check_vless.sh
     # 安装依赖
     npm install --prefix ./vless
     # 进入工作目录
     cp -r ./vless ~/domains/$USER.serv00.net
-    # 启动vless项目
-    ~/.npm-global/bin/pm2 start ~/domains/$USER.serv00.net/vless/app.js --name vless
-    # 保存pm2进程状态
-    ~/.npm-global/bin/pm2 save
 # ANSI颜色码
 GREEN='\033[0;32m'
 NC='\033[0m'  # 恢复默认颜色
@@ -99,29 +92,15 @@ echo -e "-----------------------------------------------------------------------
 echo -e "一休YouTuBe: ${GREEN}https://www.youtube.com/@yixiu001${NC}"
 echo -e "TG技术交流群: ${GREEN}https://t.me/yxjsjl${NC}"
 echo -e "--------------------------------------------------------------------------------------------------"
-echo -e "端口号: ${GREEN}${port}${NC}"
-echo -e "UUID: ${GREEN}${uuid}${NC}"
-echo -e "域名: ${GREEN}$USER.serv00.net${NC}"
-echo -e "vless进程维护定时任务脚本: ${GREEN}~/domains/$USER.serv00.net/vless/check_vless.sh${NC}"
-echo -e "VLESS节点信息: ${GREEN}vless://${uuid}@$USER.serv00.net:${port}?flow=&security=none&encryption=none&type=ws&host=$USER.serv00.net&path=/&sni=&fp=&pbk=&sid=#$USER.serv00.vless${NC}"
+echo -e "请执行以下脚本:${GREEN}cd ~/domains/$USER.serv00.net/vless && ./check_vless.sh -p <端口号>${NC}"
 }
 
 # Main function
 main() {
-    local port=3000  # Default port
-
-    while getopts "p:" opt; do
-        case $opt in
-            p) port=$OPTARG ;;
-            *) echo "无效参数"; exit 1 ;;
-        esac
-    done
-    shift $((OPTIND -1))
-
     echo "正在安装pm2..."
     install_pm2
-    echo "正在安装vless..."
-    deploy_vless "$port"
+    echo "正在部署vless到指定目录..."
+    deploy_vless
 }
 
 # 执行主函数
--- vless/check_vless.sh
+++ vless/check_vless.sh
@@ -21,20 +21,6 @@ output_yi_xiu() {
     echo -e "一休YouTuBe: ${GREEN}https://www.youtube.com/@yixiu001${NC}"
     echo -e "TG技术交流群: ${GREEN}https://t.me/yxjsjl${NC}"
     echo -e "--------------------------------------------------------------------------------------------------"
-
-    # 读取 config.json 中的 uuid 和 port
-    if [[ -f config.json ]]; then
-        uuid=$(jq -r '.uuid' config.json)
-        port=$(jq -r '.port' config.json)
-        echo -e "UUID: ${uuid}"
-        echo -e "Port: ${port}"
-        echo -e "域名: ${GREEN}$USER.serv00.net${NC}"
-        echo -e "vless进程维护定时任务脚本: ${GREEN}~/domains/$USER.serv00.net/vless/check_vless.sh${NC}"
-        echo -e "VLESS节点信息: ${GREEN}vless://${uuid}@$USER.serv00.net:${port}?flow=&security=none&encryption=none&type=ws&host=$USER.serv00.net&path=/&sni=&fp=&pbk=&sid=#$USER.serv00.vless${NC}"
-
-    else
-        echo -e "${GREEN}config.json 文件不存在或格式错误。${NC}"
-    fi
 }
 
 # 检查pm2 vless的状态
@@ -46,6 +32,26 @@ check_pm2_vless_status() {
         echo "未找到pm2 vless进程，检查是否有快照..."
         check_pm2_vless_snapshot
     fi
+}
+
+# Function to deploy vless
+deploy_vless() {
+    local port=${1:-3000}  # Default port is 3000 if not provided
+    # 修改端口号
+    save_config "$port"
+    # 安装依赖
+    npm install
+    # 启动vless项目
+    ~/.npm-global/bin/pm2 start ~/domains/$USER.serv00.net/vless/app.js --name vless
+    # 保存pm2进程状态
+    ~/.npm-global/bin/pm2 save
+    # ANSI颜色码
+    output_yi_xiu
+    echo -e "端口号: ${GREEN}${port}${NC}"
+    echo -e "UUID: ${GREEN}${uuid}${NC}"
+    echo -e "域名: ${GREEN}$USER.serv00.net${NC}"
+    echo -e "vless进程维护定时任务脚本: ${GREEN}~/domains/$USER.serv00.net/vless/check_vless.sh${NC}"
+    echo -e "VLESS节点信息: ${GREEN}vless://${uuid}@$USER.serv00.net:${port}?flow=&security=none&encryption=none&type=ws&host=$USER.serv00.net&path=/&sni=&fp=&pbk=&sid=#$USER.serv00.vless${NC}"
 }
 
 # 检查是否有pm2 vless快照
@@ -82,7 +88,43 @@ check_vless_status() {
 
 # 主函数
 main() {
+    local port=3000  # Default port
+    port_provided=false  # Flag to check if port is provided
+
+    while getopts "p:" opt; do
+        case $opt in
+            p)
+                port=$OPTARG
+                port_provided=true
+                ;;
+            *)
+                echo "无效参数"; exit 1 ;;
+        esac
+    done
+    shift $((OPTIND -1))
+
+    if $port_provided; then
+        echo "正在安装vless..."
+        deploy_vless "$port"
+        exit 1;
+    else
+        echo "没有提供-p参数，跳过vless安装。"
+    fi
+
     output_yi_xiu
+    # 读取 config.json 中的 uuid 和 port
+    if [[ -f config.json ]]; then
+        uuid=$(jq -r '.uuid' config.json)
+        port=$(jq -r '.port' config.json)
+        echo -e "UUID: ${uuid}"
+        echo -e "Port: ${port}"
+        echo -e "域名: ${GREEN}$USER.serv00.net${NC}"
+        echo -e "vless进程维护定时任务脚本: ${GREEN}~/domains/$USER.serv00.net/vless/check_vless.sh${NC}"
+        echo -e "VLESS节点信息: ${GREEN}vless://${uuid}@$USER.serv00.net:${port}?flow=&security=none&encryption=none&type=ws&host=$USER.serv00.net&path=/&sni=&fp=&pbk=&sid=#$USER.serv00.vless${NC}"
+
+    else
+        echo -e "${GREEN}config.json 文件不存在或格式错误。${NC}"
+    fi
     echo "开始检查pm2 vless进程..."
     check_pm2_vless_status
 }
