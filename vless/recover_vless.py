4bf3058a79f86756841c10884a6781bbd9ab815f
yaoyi888 <42127254+yaoyi888@users.noreply.github.com>
2024-07-21 14:30:30 +0800

    Update recover_vless.py

--- vless/recover_vless.py
+++ vless/recover_vless.py
@@ -38,7 +38,7 @@ except json.JSONDecodeError:
 summary_message = "serv00-vless 恢复操作结果：\n"
 
 # 默认恢复命令
-default_restore_command = "cd ~/domains/$USER.serv00.net/vless && ./check_vless.sh"
+default_restore_command = "pm2 resurrect"
 
 # 遍历服务器列表并执行恢复操作
 for server in servers:
